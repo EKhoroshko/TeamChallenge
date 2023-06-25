a8d8a798820f69d1aa80d1fb56591e3ec3da2101
EKhoroshko <xapoxa14@gmail.com>
2023-06-25 22:18:03 +0300

    base example

---
+++ src/enum/app-route.js
@@ -0,0 +1,8 @@
+const AppRoute = {
+    ROOT: '/',
+    LOGIN: '/login',
+    REGISTRATION: '/registration',
+    PROFILE: '/profile',
+  };
+  
+  export { AppRoute };
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { AppRoute } from './enum/app-route';
 import { Provider } from 'react-redux';
-import Home from './Pages/Home';
+import Home from './Pages/Home/Home';
+import Registration from './Pages/Registration/Registration';
+import LoginPage from './Pages/LoginPage/LoginPage';
+import ErrorPages from './Pages/ErrorPage/ErrorPages';
 import store from './redux/store';
 import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: AppRoute.ROOT,
     element: <Home />,
+    errorElement: <ErrorPages />,
+  },
+  {
+    path: AppRoute.LOGIN,
+    element: <LoginPage />,
+    errorElement: <ErrorPages />,
+  },
+  {
+    path: AppRoute.REGISTRATION,
+    element: <Registration />,
+    errorElement: <ErrorPages />,
   },
 ]);
 
