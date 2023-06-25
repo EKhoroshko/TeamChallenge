0be4869469eb5aee2e6ec495c5f16e7d0c209b05
EKhoroshko <xapoxa14@gmail.com>
2023-06-25 20:09:06 +0300

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Home from './Pages/Home';
+import store from './redux/store';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -13,6 +15,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
