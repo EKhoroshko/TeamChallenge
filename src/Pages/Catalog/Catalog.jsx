b09ab18718070894a68bf01d773f256283f0a91d
EKhoroshko <xapoxa14@gmail.com>
2023-07-09 23:36:34 +0300

    change reqvest product

--- src/Pages/Catalog/Catalog.jsx
+++ src/Pages/Catalog/Catalog.jsx
@@ -1,6 +1,6 @@
+import { useParams } from 'react-router-dom';
 import Way from './Components/Way/Way';
 import Box from './Components/Box/Box';
-import { useParams } from 'react-router-dom';
 
 const Catalog = () => {
   const params = useParams();
--- src/Pages/Catalog/Components/Pagination/Pagination.jsx
+++ src/Pages/Catalog/Components/Pagination/Pagination.jsx
@@ -1,13 +1,30 @@
+import { useSelector } from 'react-redux';
 import FilterCategory from '../FilterCategory/FilterCategory';
+import { getAllProducts } from '../../../../redux/product/selector';
+import RecomCard from '../../../Home/Component/Recomendation/RecomCard';
 import css from './Pagination.module.css';
 
 const Pagination = () => {
+  const product = useSelector(getAllProducts);
+
   return (
     <div className={css.container}>
       <div className={css.buttonFilter}>
         <FilterCategory />
       </div>
-      <div></div>
+      <ul className={css.list}>
+        {product &&
+          product.map(({ itemId, description, name, price }) => {
+            return (
+              <RecomCard
+                key={itemId}
+                description={description}
+                name={name}
+                price={price}
+              />
+            );
+          })}
+      </ul>
     </div>
   );
 };
