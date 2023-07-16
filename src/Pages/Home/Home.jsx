a62f8a89283bc3501387a12d3197ce705831b9fe
NataliiaDanchenko <danchnataly@gmail.com>
2023-07-17 01:36:27 +0300

    footer_edition_2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import HeroSection from './Component/HeroSection/HeroSection';
 import Category from './Component/Category/Category';
 import Recomendation from './Component/Recomendation/Recomendation';
 import AboutUs from './Component/AboutUs/AboutUs';
+import Reviews from "./Component/Reviews/Reviews.jsx";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -19,8 +20,9 @@ const Home = () => {
     <>
       <HeroSection />
       <Category />
-      <Recomendation product={prod} />
+        <Recomendation product={prod} />
       <AboutUs />
+        <Reviews/>
     </>
   );
 };
