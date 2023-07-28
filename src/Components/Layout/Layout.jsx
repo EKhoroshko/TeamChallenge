3f9c3c104487974b40eafaa7c8da3a0a9bc6cf1f
EKhoroshko <xapoxa14@gmail.com>
2023-07-28 04:13:58 +0300

    fix logout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { Outlet, useParams } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { refreshToken } from '../../redux/user/operation';
 import { getAll } from '../../redux/product/operation';
+import Spinner from '../Spinner/Spinner';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import css from './Layout.module.css';
@@ -29,7 +30,9 @@ const Layout = () => {
   return (
     <main className={css.main}>
       <Header />
-      <Outlet />
+      <Suspense fallback={<Spinner />}>
+        <Outlet />
+      </Suspense>
       <ToastContainer />
       <Footer />
     </main>
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,23 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { logOutUser, getFavoriteProduct } from '../../redux/user/operation';
 import { getUser } from '../../redux/user/selectors';
 import { AppRoute } from '../../enum/app-route';
+import Spinner from '../../Components/Spinner/Spinner';
 import UserMenu from './Components/UserMenu/UserMenu';
 import css from './Profile.module.css';
 
 const Profile = () => {
   const dispatch = useDispatch();
-  const { token, loading } = useSelector(getUser);
+  const { token } = useSelector(getUser);
   const navigate = useNavigate();
 
-  const handlelogOut = () => {
-    dispatch(logOutUser(token));
+  const handlelogOut = async () => {
+    await dispatch(logOutUser(token));
     localStorage.removeItem('token');
-    if (!loading) {
-      navigate(AppRoute.ROOT);
-    }
+    navigate(AppRoute.ROOT);
   };
 
   useEffect(() => {
@@ -34,7 +33,9 @@ const Profile = () => {
           <button className={css.button} onClick={handlelogOut}>
             logOut
           </button>
-          <Outlet />
+          <Suspense fallback={<Spinner />}>
+            <Outlet />
+          </Suspense>
         </div>
       </div>
     </section>
