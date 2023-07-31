c3c034f72934096b2bdcf0347c8f906e1a251c4f
NataliiaDanchenko <danchnataly@gmail.com>
2023-07-31 03:07:07 +0300

    form_validation

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -9,6 +9,8 @@ import { loginUser, registrationUser } from '../../redux/user/operation';
 import { getLoadingUser, getUser } from '../../redux/user/selectors';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './LoginPage.module.css';
+import Joi from "joi";
+
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -21,6 +23,70 @@ const LoginPage = () => {
     password: '',
     username: '',
   });
+
+
+  { /* Начало валидации формы. Функция срабатывает по нажатию на кнопку */ }
+
+  const schema = Joi.object().keys({
+    password: Joi.string()
+        .min(5)
+        .max(100)
+        .required(),
+    email: Joi.string()
+        .min(5)
+        .max(200)
+        .required()
+  });
+
+  const [errors, setErrors] = useState([]);
+
+  function validationField(schema, value, field) {
+    const err = JSON.parse(JSON.stringify(errors));
+    console.log(err)
+    const res = schema.validate(value);
+    console.log(res)
+    console.log(value)
+    console.log(res)
+    let errorsList = {};
+    if (res.error) {
+      res.error.details.forEach((error) => {
+        errorsList[field] = error.message;
+        console.log(error.message);
+        setTimeout(() => alert('Email and Password must be at least 5 characters long'), 0);
+      });
+      setErrors({
+        ...errors,
+        ...errorsList
+      });
+    }
+    else {
+      delete err[field];
+      setErrors(err);
+    }
+  }
+
+  function handleChange1(email, password) {
+    const payload = {
+      email,
+      password
+    };
+    validationField(schema, payload);
+    console.log(schema)
+  }
+
+
+  function validationPaylod(payload) {
+  handleChange1();
+    const res = schema.validate(payload);
+    if (res.error) {
+      console.log(errors)
+    } else {
+      console.log('ok!')
+    }
+  }
+
+  { /* Конец валидации формы */ }
+
 
   useEffect(() => {
     if (user.token) {
@@ -81,6 +147,7 @@ const LoginPage = () => {
       {loader}
       <div className={css.box}>
         <div className={css.btnBox}>
+
           <Button
             active={css.btnActive}
             onClick={e => handleValue(e)}
@@ -114,7 +181,7 @@ const LoginPage = () => {
               value={form.password}
               onChange={handleChange}
             />
-            <button type="submit" className={css.btn}>
+            <button type="submit" className={css.btn} onClick={validationPaylod}>
               Логин
             </button>
           </form>
@@ -144,7 +211,7 @@ const LoginPage = () => {
               value={form.password}
               onChange={handleChange}
             />
-            <button type="submit" className={css.btn}>
+            <button type="submit" className={css.btn} onClick={validationPaylod}>
               Регистрация
             </button>
           </form>
